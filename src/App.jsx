a433d4863bef3f6c1b16d2733d5054a35f822a8c
Daniel Obazee <obazeedaniel03@gmail.com>
2025-12-26 14:16:04 -0800

    Feat: addedinteractive example section with TabButton and useState. Implemented useState to manage selected example topic Enhanced TabButton to handle onSelect prop for dynamic tab switching Displayed example content based on selected topic for improved interactivity

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import { CORE_CONCEPTS } from "./data.js";
 import { Header } from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import { TabButton } from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data.js";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+  const [selectedTopic, setSelectedTopic] = useState("components");
 
   function handleClick(selectedButton) {
     // selectedButton => "components", "jsx", "props", "states";
@@ -45,7 +46,14 @@ function App() {
             <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
             <TabButton onSelect={() => handleClick("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
